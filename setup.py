9d54707e03fcab94b3ef645a5dc459b4507ac5b9
Clemens Winter <clemenswinter1@gmail.com>
2021-11-20 15:01:31 -0800

    Simplified API, mypy, more docs

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@
 
 from distutils.core import setup
 
-setup(name='entity-gym',
-      version='1.0',
-      description='Entity Gym',
-      author='Clemens Winter',
-      author_email='clemenswinter1@gmail.com',
-      packages=['entity_gym'],
-      )
+setup(
+    name="entity-gym",
+    version="1.0",
+    description="Entity Gym",
+    author="Clemens Winter",
+    author_email="clemenswinter1@gmail.com",
+    packages=["entity_gym"],
+)
