4515bcf0b9ae064a8f3ae5b127cceddf0ca0cfdd
Costa Huang <costa.huang@outlook.com>
2021-11-29 18:32:16 -0500

    Layer initialization fix (#56)
    * Layer initialization fix
    
    * fix type error
    
    * fix ci
    
    * reformat
    
    * ok this should fix it
    
    * final fix

--- rogue_net/rogue_net/head_creator.py
+++ rogue_net/rogue_net/head_creator.py
@@ -1,19 +1,27 @@
 from typing import Dict
 from torch import nn
-
+import torch
+import numpy as np
 from entity_gym.environment import ActionSpace, CategoricalActionSpace
+from typing import Dict
+
+
+def layer_init(
+    layer: nn.Module, std: float = np.sqrt(2), bias_const: float = 0.0,
+) -> nn.Module:
+    torch.nn.init.orthogonal_(layer.weight, std)
+    torch.nn.init.constant_(layer.bias, bias_const)  # type: ignore
+    return layer
 
 
 def create_head_for(space: ActionSpace, d_model: int) -> nn.Module:
     if isinstance(space, CategoricalActionSpace):
-        return nn.Linear(d_model, len(space.choices))
+        return layer_init(nn.Linear(d_model, len(space.choices)), std=0.01)
     raise NotImplementedError()
 
 
 def create_value_head(d_model: int) -> nn.Module:
-    value_head = nn.Linear(d_model, 1)
-    value_head.weight.data.fill_(0.0)
-    value_head.bias.data.fill_(0.0)
+    value_head = layer_init(nn.Linear(d_model, 1), std=1.0)
     return value_head
 
 
