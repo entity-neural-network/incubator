3b785b8930249dc75c6d87491885a3a3aa09368d
Bam4d <chrisbam4d@gmail.com>
2021-12-01 12:56:56 +0000

    have to run  using

--- rogue_net/rogue_net/head_creator.py
+++ rogue_net/rogue_net/head_creator.py
@@ -7,7 +7,9 @@ from typing import Dict
 
 
 def layer_init(
-    layer: nn.Module, std: float = np.sqrt(2), bias_const: float = 0.0,
+    layer: nn.Module,
+    std: float = np.sqrt(2),
+    bias_const: float = 0.0,
 ) -> nn.Module:
     torch.nn.init.orthogonal_(layer.weight, std)
     torch.nn.init.constant_(layer.bias, bias_const)  # type: ignore
