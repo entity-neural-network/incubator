90bdc7d3355e11438b586adba5f0ba46a317a03b
Bam4d <chrisbam4d@gmail.com>
2021-12-02 10:17:12 +0000

    ordering is inconsistent across machines, this will probably result in flakey tests until this is fixed

--- enn_zoo/enn_zoo/griddly/test_griddly_env.py
+++ enn_zoo/enn_zoo/griddly/test_griddly_env.py
@@ -13,48 +13,48 @@ def test_griddly_wrapper() -> None:
     # Check the observation space is being created correctly from the test environment
     observation_space = env_class.obs_space()
     assert len(observation_space.entities) == 3
-    assert set(observation_space.entities["__global__"].features) == {
+    assert observation_space.entities["__global__"].features == [
         "_steps",
         "test_global_variable",
-    }
+    ]
 
     # TODO: currently we pass all possible variables to each feature, this should be fixed once features API is in a more consistent state
-    assert set(observation_space.entities["entity_1"].features) == {
+    assert observation_space.entities["entity_1"].features == [
         "x",
         "y",
         "z",
         "orientation",
         "player_id",
-        "entity_2_variable",
         "entity_1_variable",
-    }
-    assert set(observation_space.entities["entity_2"].features) == {
+        "entity_2_variable"
+    ]
+    assert observation_space.entities["entity_2"].features == [
         "x",
         "y",
         "z",
         "orientation",
         "player_id",
-        "entity_2_variable",
         "entity_1_variable",
-    }
+        "entity_2_variable",
+    ]
 
     # Check the action space is being created correctly fro the test environment
     action_space = env_class.action_space()
     assert isinstance(action_space["move_one"], CategoricalActionSpace)
-    assert set(action_space["move_one"].choices) == {
+    assert action_space["move_one"].choices == [
         "NOP",
         "Left",
         "Up",
         "Right",
         "Down",
-    }
+    ]
     assert isinstance(action_space["move_two"], CategoricalActionSpace)
-    assert set(action_space["move_two"].choices) == {
+    assert action_space["move_two"].choices == [
         "NOP",
         "Do a little dance",
         "Make a little love",
         "Get down tonight",
-    }
+    ]
 
     # Check that observation is created correctly
     env = env_class()
@@ -62,17 +62,17 @@ def test_griddly_wrapper() -> None:
 
     # Check the entities in the observation
     print(observation.entities["entity_1"])
-    print(np.array([[2, 2, 0, 0, 1, 0, 5]], dtype=np.float32))
+    print(np.array([[2, 2, 0, 0, 1, 5, 0]], dtype=np.float32))
     assert np.all(
         observation.entities["entity_1"]
-        == np.array([[2, 2, 0, 0, 1, 0, 5]], dtype=np.float32)
+        == np.array([[2, 2, 0, 0, 1, 5, 0]], dtype=np.float32)
     )
 
     print(np.sort(observation.entities["entity_2"], axis=0))
-    print(np.array([[2, 3, 0, 0, 0, 10, 0], [4, 4, 0, 0, 0, 10, 0]], dtype=np.float32))
+    print(np.array([[2, 3, 0, 0, 0, 0, 10], [4, 4, 0, 0, 0, 0, 10]], dtype=np.float32))
     assert np.all(
         np.sort(observation.entities["entity_2"], axis=0)
-        == np.array([[2, 3, 0, 0, 0, 10, 0], [4, 4, 0, 0, 0, 10, 0]], dtype=np.float32)
+        == np.array([[2, 3, 0, 0, 0, 0, 10], [4, 4, 0, 0, 0, 0, 10]], dtype=np.float32)
     )
 
     # Check the masks in the observation
