d9d9989f7dd6f6bd972b0aee2e41e52209bd4c60
Costa Huang <costa.huang@outlook.com>
2022-01-01 17:11:06 -0500

    Prototype Microrts support

---
+++ enn_zoo/enn_zoo/microrts/__init__.py
@@ -0,0 +1,222 @@
+from dataclasses import dataclass
+from typing import Dict, List, Mapping, Sequence, Tuple
+import random
+import numpy as np
+from copy import deepcopy
+import os
+import gym_microrts
+from gym_microrts import microrts_ai
+import xml.etree.ElementTree as ET
+import json
+
+from entity_gym.environment import (
+    CategoricalAction,
+    DenseCategoricalActionMask,
+    Entity,
+    Environment,
+    EpisodeStats,
+    ObsSpace,
+    Type,
+    CategoricalActionSpace,
+    ActionSpace,
+    Observation,
+    Action,
+    VecEnv,
+)
+
+import jpype
+from jpype.imports import registerDomain
+import jpype.imports
+from jpype.types import JArray, JInt
+
+
+class GymMicrorts(Environment):
+    """
+    Turn-based version of Snake with multiple snakes.
+    Each snake has a different color.
+    For each snake, Food of that color is placed randomly on the board.
+    Snakes can only eat Food of their color.
+    When a snake eats Food of the same color, it grows by one unit.
+    When a snake grows and it's length was less than 11, the player receives a reward of 0.1 / num_snakes.
+    The game ends when a snake collides with another snake, runs into a wall, eats Food of another color, or all snakes reach a length of 11.
+    """
+
+    def __init__(
+        self,
+        map_paths=["maps/10x10/basesTwoWorkers10x10.xml"],
+        partial_obs=False,
+        reward_weight=np.array([0.0, 1.0, 0.0, 0.0, 0.0, 5.0]),
+    ):
+        self.map_paths = map_paths
+        self.partial_obs = partial_obs
+        self.reward_weight = reward_weight
+
+        self.step = 0
+
+        # read map
+        self.microrts_path = os.path.join(gym_microrts.__path__[0], "microrts")
+        root = ET.parse(os.path.join(self.microrts_path, self.map_paths[0])).getroot()
+        self.height, self.width = int(root.get("height")), int(root.get("width"))
+
+        # launch the JVM
+        if not jpype._jpype.isStarted():
+            registerDomain("ts", alias="tests")
+            registerDomain("ai")
+            jars = [
+                "microrts.jar",
+                "lib/bots/Coac.jar",
+                "lib/bots/Droplet.jar",
+                "lib/bots/GRojoA3N.jar",
+                "lib/bots/Izanagi.jar",
+                "lib/bots/MixedBot.jar",
+                "lib/bots/TiamatBot.jar",
+                "lib/bots/UMSBot.jar",
+                "lib/bots/mayariBot.jar",  # "MindSeal.jar"
+            ]
+            for jar in jars:
+                print(os.path.join(self.microrts_path, jar))
+                jpype.addClassPath(os.path.join(self.microrts_path, jar))
+            jpype.startJVM(convertStrings=False)
+
+        # start microrts client
+        from rts.units import UnitTypeTable
+
+        self.real_utt = UnitTypeTable()
+        from ai.rewardfunction import (
+            RewardFunctionInterface,
+            WinLossRewardFunction,
+            ResourceGatherRewardFunction,
+            AttackRewardFunction,
+            ProduceWorkerRewardFunction,
+            ProduceBuildingRewardFunction,
+            ProduceCombatUnitRewardFunction,
+            CloserToEnemyBaseRewardFunction,
+        )
+
+        self.rfs = JArray(RewardFunctionInterface)(
+            [
+                WinLossRewardFunction(),
+                ResourceGatherRewardFunction(),
+                ProduceWorkerRewardFunction(),
+                ProduceBuildingRewardFunction(),
+                AttackRewardFunction(),
+                ProduceCombatUnitRewardFunction(),
+                # CloserToEnemyBaseRewardFunction(),
+            ]
+        )
+
+        self.ai2s = [microrts_ai.coacAI for _ in range(1)]
+
+        from ts.entity import JNIEntityClient as Client
+        from ai.core import AI
+
+        self.client = Client(
+            self.rfs,
+            os.path.expanduser(self.microrts_path),
+            self.map_paths[0],
+            self.ai2s[0](self.real_utt),
+            self.real_utt,
+            self.partial_obs,
+        )
+        # get the unit type table
+        self.utt = json.loads(str(self.client.sendUTT()))
+
+    @classmethod
+    def obs_space(cls) -> ObsSpace:
+        return ObsSpace(
+            {
+                "Resource": Entity(["x", "y"]),
+                "Base": Entity(["x", "y"]),
+                "Barracks": Entity(["x", "y"]),
+                "Worker": Entity(["x", "y"]),
+                # "Light": Entity(["x", "y"]),
+                # "Heavy": Entity(["x", "y"]),
+                # "Ranged": Entity(["x", "y"]),
+            }
+        )
+
+    @classmethod
+    def action_space(cls) -> Dict[str, ActionSpace]:
+        return {
+            "move": CategoricalActionSpace(
+                choices=["up", "right", "down", "left"],
+            ),
+            # "harvest": CategoricalActionSpace(
+            #     choices=["up", "right", "down", "left"],
+            # ),
+        }
+
+    def _reset(self) -> Observation:
+        self.step = 0
+        return self._observe()
+
+    def _act(self, action: Mapping[str, Action]) -> Observation:
+        game_over = False
+        self.step += 1
+
+        print("=========", action)
+        if "move" in action:
+            response = self.client.gameStep(action["move"].actions, 0)
+        else:
+            response = self.client.gameStep([], 0)
+        self.client.render(False)
+        return Observation(
+            entities={
+                "Resource": np.array(response.observation[0]).astype(np.float32),
+                "Base": np.array(response.observation[1]).astype(np.float32),
+                # "Barracks": np.array(response.observation[2]).astype(np.float32),
+                "Worker": np.array(response.observation[3]).astype(np.float32),
+                # "Light": np.array(response.observation[4]).astype(np.float32),
+                # "Heavy": np.array(response.observation[5]).astype(np.float32),
+                # "Ranged": np.array(response.observation[6]).astype(np.float32),
+            },
+            ids=np.array(response.observation[7]),
+            action_masks={
+                "move": DenseCategoricalActionMask(
+                    actors=np.array(response.observation[8]),
+                ),
+                # "harvest": DenseCategoricalActionMask(
+                #     actors=[
+                #         4,
+                #     ],
+                # ),
+            },
+            reward=response.reward @ self.reward_weight,
+            done=response.done[0],
+            end_of_episode_info=EpisodeStats(length=self.step, total_reward=1)
+            if response.done[0]
+            else None,
+        )
+
+    def _observe(self, done: bool = False, player: int = 0) -> Observation:
+        response = self.client.reset(0)
+        self.client.render(False)
+        print(np.array(response.observation[7]))
+        print(np.array(response.observation[8]))
+        return Observation(
+            entities={
+                "Resource": np.array(response.observation[0]).astype(np.float32),
+                "Base": np.array(response.observation[1]).astype(np.float32),
+                # "Barracks": np.array(response.observation[2]).astype(np.float32),
+                "Worker": np.array(response.observation[3]).astype(np.float32),
+                # "Light": np.array(response.observation[4]).astype(np.float32),
+                # "Heavy": np.array(response.observation[5]).astype(np.float32),
+                # "Ranged": np.array(response.observation[6]).astype(np.float32),
+            },
+            ids=np.array(response.observation[7]),
+            action_masks={
+                "move": DenseCategoricalActionMask(
+                    actors=np.array(response.observation[8]),
+                ),
+                # "harvest": DenseCategoricalActionMask(
+                #     actors=[
+                #         4,
+                #     ],
+                # ),
+            },
+            reward=response.reward @ self.reward_weight,
+            done=response.done[0],
+            end_of_episode_info=EpisodeStats(length=self.step, total_reward=1)
+            if response.done[0]
+            else None,
+        )
---
+++ enn_zoo/enn_zoo/microrts/main.py
@@ -0,0 +1,77 @@
+import argparse
+from typing import Dict, Type
+from enn_zoo.microrts import GymMicrorts
+
+from entity_gym.environment import (
+    CategoricalAction,
+    DenseSelectEntityActionMask,
+    Environment,
+    ObsSpace,
+    CategoricalActionSpace,
+    Action,
+    Observation,
+    SelectEntityAction,
+    SelectEntityActionSpace,
+)
+
+
+def print_obs(obs: Observation, total_reward: float, obs_filter: ObsSpace) -> None:
+    print(f"Reward: {obs.reward}")
+    print(f"Total reward: {total_reward}")
+    entity_index = 0
+    for entity_type, features in obs.entities.items():
+        for entity in range(features.shape[0]):
+            print(
+                f"{obs.ids[entity_index]}: {entity_type}({', '.join(map(lambda nv: nv[0] + '=' + str(nv[1]), zip(obs_filter.entities[entity_type].features, features[entity, :])))})"
+            )
+            entity_index += 1
+
+
+if __name__ == "__main__":
+    env_cls = GymMicrorts
+
+    print(env_cls)
+    env = env_cls()
+    obs_space = env_cls.obs_space()
+    print(obs_space)
+    actions = env_cls.action_space()
+    obs = env.reset(obs_space)
+    total_reward = obs.reward
+    while not obs.done:
+        print_obs(obs, total_reward, obs_space)
+        action: Dict[str, Action] = {}
+        for action_name, action_mask in obs.action_masks.items():
+            action_def = actions[action_name]
+            for actor_id in action_mask.actors:
+                if isinstance(action_def, CategoricalActionSpace):
+                    # Prompt user for action
+                    print(f"Choose {action_name} for {actor_id}")
+                    for i, label in enumerate(action_def.choices):
+                        print(f"{i}) {label}")
+                    choice_id = int(input())
+                    if action_name not in action:
+                        action[action_name] = CategoricalAction([])
+                    action[action_name].actions.append((actor_id, choice_id))
+                elif isinstance(action_def, SelectEntityActionSpace):
+                    assert isinstance(action_mask, DenseSelectEntityActionMask)
+                    # Prompt user for entity
+                    print(f"{action_name}")
+                    if action_mask.mask is not None:
+                        print(
+                            f"Selectable entities: {', '.join([str(obs.ids[i]) for i, selectable in enumerate(action_mask.mask) if selectable])}"
+                        )
+                    else:
+                        print("Selectable entities: all")
+                    entity_id = int(input())
+                    if action_name not in action:
+                        action[action_name] = SelectEntityAction([])
+                    action[action_name].actions.append((actor_id, entity_id))
+                else:
+                    raise ValueError(f"Unknown action type {action_def}")
+        # print(action)
+        # raise
+        obs = env.act(action, obs_space)
+        total_reward += obs.reward
+
+    print_obs(obs, total_reward, obs_space)
+    print("Episode finished")
