bf13888c466bd21efa204f6863ebf7a931c70361
Clemens Winter <clemenswinter1@gmail.com>
2022-01-18 20:27:38 -0800

    more fix

--- enn_ppo/enn_ppo/train.py
+++ enn_ppo/enn_ppo/train.py
@@ -400,31 +400,32 @@ def returns_and_advantages(
 ) -> Tuple[torch.Tensor, torch.Tensor]:
     # bootstrap value if not done
     next_value = agent.get_value(next_obs.entities, tracer).reshape(1, -1)
-    if args.gae:
+    num_steps = values.size(0)
+    if gae:
         advantages = torch.zeros_like(rewards).to(device)
         lastgaelam = 0
-        for t in reversed(range(args.num_steps)):
-            if t == args.num_steps - 1:
+        for t in reversed(range(num_steps)):
+            if t == num_steps - 1:
                 nextnonterminal = 1.0 - next_done.float()
                 nextvalues = next_value
             else:
                 nextnonterminal = 1.0 - dones[t + 1]
                 nextvalues = values[t + 1]
-            delta = rewards[t] + args.gamma * nextvalues * nextnonterminal - values[t]
+            delta = rewards[t] + gamma * nextvalues * nextnonterminal - values[t]
             advantages[t] = lastgaelam = (
-                delta + args.gamma * args.gae_lambda * nextnonterminal * lastgaelam
+                delta + gamma * gae_lambda * nextnonterminal * lastgaelam
             )
         returns = advantages + values
     else:
         returns = torch.zeros_like(rewards).to(device)
-        for t in reversed(range(args.num_steps)):
-            if t == args.num_steps - 1:
+        for t in reversed(range(num_steps)):
+            if t == num_steps - 1:
                 nextnonterminal = 1.0 - next_done
                 next_return = next_value
             else:
                 nextnonterminal = 1.0 - dones[t + 1]
                 next_return = returns[t + 1]
-            returns[t] = rewards[t] + args.gamma * nextnonterminal * next_return
+            returns[t] = rewards[t] + gamma * nextnonterminal * next_return
         advantages = returns - values
     return returns, advantages
 
