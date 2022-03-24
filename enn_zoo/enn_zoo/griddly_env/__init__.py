87454968dadc44c9bf8712e0b72d439b2b62e993
Bam4d <chrisbam4d@gmail.com>
2022-03-24 07:37:27 +0000

    black

--- enn_zoo/enn_zoo/griddly_env/__init__.py
+++ enn_zoo/enn_zoo/griddly_env/__init__.py
@@ -55,13 +55,13 @@ def generate_action_space(env: Any) -> Dict[str, ActionSpace]:
 
 
 def create_env(
-        yaml_file: str,
-        global_observer_type: Any = gd.ObserverType.BLOCK_2D,
-        image_path: Optional[str] = None,
-        shader_path: Optional[str] = None,
-        level: int = 0,
-        random_levels: bool = False,
-        level_generator: Optional[LevelGenerator] = None,
+    yaml_file: str,
+    global_observer_type: Any = gd.ObserverType.BLOCK_2D,
+    image_path: Optional[str] = None,
+    shader_path: Optional[str] = None,
+    level: int = 0,
+    random_levels: bool = False,
+    level_generator: Optional[LevelGenerator] = None,
 ) -> Type[GriddlyEnv]:
     """
     In order to fit the API for the Environment, we need to pre-load the environment from the yaml and then pass in
